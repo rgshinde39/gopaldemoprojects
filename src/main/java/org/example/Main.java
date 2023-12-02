4668878c9ff5712447e42e89285ed41dbb73e0a3
gopal6148 <132437812+gopal6148@users.noreply.github.com>
2023-12-02 14:09:49 +0530

    Added method add two numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Test tt = new Test();
+      int result =  tt.addTwoNum(10, 20);
+      System.out.println(result);
     }
 }
---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Test {
+	public int addTwoNum(int a, int b) {
+		return a+b;
+	}
+
+}
